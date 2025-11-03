6d2ca3a49ac6997d6875bbcc7844d704895e837c
Yosssef9 <yossefyasser561@example.com>
2025-11-03 17:46:18 +0200

    edit config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // ده يخلي السيرفر يستمع لكل الـ IPs مش بس localhost
+    port: 5173, // البورت اللي انت عايزه، ممكن تغيره
+  },
+});
